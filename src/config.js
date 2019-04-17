0fa9c3762084cf1f7e8de5552db6fd8d6ad646e8
Thibaud Colas <thibaudcolas@gmail.com>
2019-04-17 01:28:51 +0100

    feat(config): add a11y-related rules. Fix #18
    BREAKING CHANGE: this adds 6 new rules related to accessibility.

--- src/config.js
+++ src/config.js
@@ -8,6 +8,7 @@ module.exports = {
     "stylelint-declaration-block-no-ignored-properties",
     "stylelint-declaration-strict-value",
     "stylelint-no-unsupported-browser-features",
+    "stylelint-a11y",
   ],
   rules: {
     "color-named": "never",
@@ -55,5 +56,11 @@ module.exports = {
         ],
       },
     ],
+    "a11y/content-property-no-static-value": true,
+    "a11y/font-size-is-readable": true,
+    "a11y/media-prefers-reduced-motion": true,
+    "a11y/no-obsolete-attribute": true,
+    "a11y/no-obsolete-element": true,
+    "a11y/no-text-align-justify": true,
   },
 };
