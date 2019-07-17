5ea2e2e18c0d1593c6cafad4f65af850f9722c67
Thibaud Colas <thibaudcolas@gmail.com>
2019-07-17 23:22:07 +0100

    chore(qa): simplify pre-commit hook definitions

--- .githooks/pre-commit.7.lint.sh
+++ .githooks/pre-commit.7.lint.sh
@@ -1,30 +1,5 @@
 #!/usr/bin/env bash
 
-if [ -n "$JS_STAGED" ];
-then
-    npx prettier --check $JS_STAGED
-fi
-
-if [ -n "$SCSS_STAGED" ];
-then
-    npx prettier --check $SCSS_STAGED
-fi
-
-if [ -n "$MD_STAGED" ];
-then
-    npx prettier --check $MD_STAGED
-fi
-
-if [ -n "$JSON_STAGED" ];
-then
-    npx prettier --check $JSON_STAGED
-fi
-
-if [ -n "$YAML_STAGED" ];
-then
-    npx prettier --check $YAML_STAGED
-fi
-
 TRAVIS_STAGED=$(grep -e '.travis.yml$' -e '.travis.yaml$' <<< "$STAGED" || true)
 
 if [ -n "$TRAVIS_STAGED" ];
